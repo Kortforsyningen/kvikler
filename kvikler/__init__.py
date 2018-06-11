e8f5505d0be8e72c9c685886ebbd7003f4048e7e
Kristian Evers <kristianevers@gmail.com>
2018-06-11 15:00:58 +0200

    Convert to python3

--- kvikler/__init__.py
+++ kvikler/__init__.py
@@ -1,5 +1,7 @@
 import os
 import json
+import getpass
+from pathlib import Path
 
 # Used for controlling the database setup when running the test suite
 IS_TESTING = False
@@ -7,25 +9,29 @@ RC_NAME = 'kvikler_settings.json'
 
 __version__ = '0.0.1'
 
+if os.environ.get('HOME'):
+    home = Path.os.environ['HOME']
+else:
+    home = Path('')
 
-# Possible locations for configuration file
 search_files = [
-    # HOME is usually either /home/user or C:\Users\user
-    os.path.join(os.environ.get('HOME'), '.' + RC_NAME), # with a dot for unix users
-    os.path.join(os.environ.get('HOME'), RC_NAME), # without a dot for windows users
-    os.path.join('/etc', RC_NAME),
-    os.path.join('C:\\Users\\Default\\AppData\\Local\\kvikler', RC_NAME),
+    home / Path(RC_NAME),
+    home / Path('.' + RC_NAME),
+    Path('/etc') / Path(RC_NAME),
+    Path('C:\\Users') / Path(getpass.getuser()) / Path(RC_NAME),
+    Path('C:\\Users\\Default\\AppData\\Local\\kvikler') / Path(RC_NAME),
 ]
+
 
 for conf_file in search_files:
     if os.path.isfile(conf_file):
         with open(conf_file) as conf:
             settings = json.load(conf)
-            if settings.has_key('connection'):
+            if 'connection' in settings:
                 conf_db = settings['connection']
-                if not (conf_db.has_key('username') and conf_db.has_key('password') and
-                        conf_db.has_key('hostname') and conf_db.has_key('database') and
-                        conf_db.has_key('schema')):
+                if not ('username' in conf_db and 'password' in conf_db and
+                        'hostname' in conf_db and 'database' in conf_db and
+                        'schema' in conf_db):
                     raise ValueError('Malformed config file. Consult documentation!')
         break
 else:
@@ -36,7 +42,7 @@ password = conf_db['password']
 hostname = conf_db['hostname']
 database = conf_db['database']
 schema = conf_db['schema']
-if conf_db.has_key('port'):
+if 'port' in conf_db:
     port = conf_db['port']
 else:
     port = 5432
